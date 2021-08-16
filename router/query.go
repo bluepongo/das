182f83e61af058920bc2c3cc698f91738bed61fa
Romber Li <romber2001@163.com>
2021-08-16 13:41:23 +0800

    query: add router

---
+++ router/query.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/romberli/das/api/v1/query"
+)
+
+// RegisterQuery is the sub-router of das for query
+func RegisterQuery(group *gin.RouterGroup) {
+	queryGroup := group.Group("/query")
+	{
+		queryGroup.GET("/all", query.GetAll)
+		queryGroup.POST("/get-by-db-id/:dbID", query.GetByDBID)
+		queryGroup.POST("/get-by-id/:id", query.GetByID)
+	}
+}
