b3af7c9d96d53592bf58d2f6d45e874f2dda91de
zetatez <zetatez@icloud.com>
2021-08-30 16:01:14 +0800

    load data: not null to default null

--- sql/v1__base_version.sql
+++ sql/v1__base_version.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `t_meta_app_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `app_name` varchar(100) NOT NULL COMMENT '应用系统名称',
-  `level` tinyint(4) NOT NULL COMMENT '系统等级: 1-A, 2-B, 3-C',
+  `level` tinyint(4) DEFAULT NULL COMMENT '系统等级: 1-A, 2-B, 3-C',
   `owner_id` int(11) DEFAULT NULL COMMENT '应用系统主要负责人ID',
   `del_flag` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除标记: 0-未删除, 1-已删除',
   `create_time` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
@@ -132,7 +132,7 @@ CREATE TABLE `t_meta_mysql_server_info` (
 CREATE TABLE `t_meta_user_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
   `user_name` varchar(100) NOT NULL COMMENT '姓名',
-  `department_name` varchar(100) NOT NULL COMMENT '部门/团队名称',
+  `department_name` varchar(100) DEFAULT NULL COMMENT '部门/团队名称',
   `employee_id` varchar(100) DEFAULT NULL COMMENT '工号',
   `account_name` varchar(100) NOT NULL COMMENT '账号名称',
   `email` varchar(100) NOT NULL COMMENT '邮箱',
