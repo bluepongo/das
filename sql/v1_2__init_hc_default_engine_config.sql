3921c5c24b28474ddd6495e57914f3a1e5a23e68
Romber Li <romber2001@163.com>
2021-09-24 17:18:26 +0800

    alert: modify sql file

--- sql/v1_2__init_hc_default_engine_config.sql
+++ sql/v1_2__init_hc_default_engine_config.sql
@@ -1,13 +1,17 @@
 insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
 values('db_config', 5, 0, 0, 0, 10, 50, 0, 0);
 insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
+values('avg_backup_failed_ratio', 5, 0.1, 0.2, 0.1, 20, 100, 10, 50);
+insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
+values('statistics_failed_ratio', 5, 0.1, 0.2, 0.1, 20, 100, 10, 50);
+insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
 values('cpu_usage', 5, 0.5, 0.8, 0.1, 20, 100, 10, 50);
 insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
 values('io_usage', 5, 0.5, 0.8, 0.1, 20, 100, 10, 50);
 insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
 values('disk_capacity_usage', 20, 0.5, 0.8, 0.1, 40, 100, 10, 50);
 insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
-values('connection_usage', 20, 0.5, 0.8, 0.1, 40, 100, 10, 50);
+values('connection_usage', 10, 0.5, 0.8, 0.1, 40, 100, 10, 50);
 insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
 values('average_active_session_percents', 10, 0.1, 0.3, 0.1, 20, 100, 10, 50);
 insert into t_hc_default_engine_config(item_name, item_weight, low_watermark, high_watermark, unit, score_deduction_per_unit_high, max_score_deduction_high, score_deduction_per_unit_medium, max_score_deduction_medium)
