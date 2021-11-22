329a1b8d304ce564a5b8215e2a0d4c9d6efb67aa
Romber Li <romber2001@163.com>
2021-11-22 17:07:25 +0800

    query: modify unit test

--- internal/app/query/repository_test.go
+++ internal/app/query/repository_test.go
@@ -1,112 +1,183 @@
 package query
 
 import (
+	"fmt"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/romberli/das/config"
+	"github.com/romberli/das/global"
+	"github.com/romberli/das/internal/app/metadata"
+	"github.com/romberli/go-util/common"
 	"github.com/romberli/go-util/constant"
+	"github.com/romberli/go-util/middleware/clickhouse"
+	"github.com/romberli/go-util/middleware/mysql"
+	"github.com/romberli/log"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	testDASMySQLAddr = "192.168.10.219:3306"
+	testDASMySQLName = "das"
+	testDASMySQLUser = "root"
+	testDASMySQLPass = "root"
+)
+
+var (
+	testDASRepo        *DASRepo
+	testMySQLRepo      *MySQLRepo
+	testClickhouseRepo *ClickhouseRepo
+)
+
+func init() {
+	testInitDASMySQLPool()
+	testInitViper()
+
+	testDASRepo = NewDASRepoWithGlobal()
+	testMySQLRepo = testInitMySQLRepo()
+	testClickhouseRepo = testInitClickhouseRepo()
+}
+
+func testInitDASMySQLPool() {
+	var err error
+
+	if global.DASMySQLPool == nil {
+		global.DASMySQLPool, err = mysql.NewPoolWithDefault(testDASMySQLAddr, testDASMySQLName, testDASMySQLUser, testDASMySQLPass)
+		if err != nil {
+			log.Error(common.CombineMessageWithError("testInitDASMySQLPool() failed", err))
+			os.Exit(constant.DefaultAbnormalExitCode)
+		}
+	}
+}
+
+func testInitViper() {
+	viper.Set(config.DBMonitorMySQLUserKey, config.DefaultDBMonitorMySQLUser)
+	viper.Set(config.DBMonitorMySQLPassKey, config.DefaultDBMonitorMySQLPass)
+	viper.Set(config.DBMonitorClickhouseUserKey, config.DefaultDBMonitorClickhouseUser)
+	viper.Set(config.DBMonitorClickhousePassKey, config.DefaultDBMonitorClickhousePass)
+	viper.Set(config.QueryMinRowsExaminedKey, 1)
+}
+
+func testInitMySQLRepo() *MySQLRepo {
+	mysqlServerService := metadata.NewMySQLServerServiceWithDefault()
+	err := mysqlServerService.GetByID(testPMM1MySQLServerID)
+	if err != nil {
+		log.Error(common.CombineMessageWithError("testInitMySQLRepo() failed", err))
+		os.Exit(constant.DefaultAbnormalExitCode)
+	}
+	monitorSystem, err := mysqlServerService.GetMySQLServers()[constant.ZeroInt].GetMonitorSystem()
+	if err != nil {
+		log.Error(common.CombineMessageWithError("testInitMySQLRepo() failed", err))
+		os.Exit(constant.DefaultAbnormalExitCode)
+	}
+	addr := fmt.Sprintf("%s:%d", monitorSystem.GetHostIP(), monitorSystem.GetPortNumSlow())
+	dbUser := viper.GetString(config.DBMonitorMySQLUserKey)
+	dbPass := viper.GetString(config.DBMonitorMySQLPassKey)
+	conn, err := mysql.NewConn(addr, pmmMySQLDBName, dbUser, dbPass)
+	if err != nil {
+		log.Error(common.CombineMessageWithError("testInitMySQLRepo() failed", err))
+		os.Exit(constant.DefaultAbnormalExitCode)
+	}
+
+	return NewMySQLRepo(NewConfigWithDefault(), conn)
+}
+
+func testInitClickhouseRepo() *ClickhouseRepo {
+	mysqlServerService := metadata.NewMySQLServerServiceWithDefault()
+	err := mysqlServerService.GetByID(testPMM2MySQLServerID)
+	if err != nil {
+		log.Error(common.CombineMessageWithError("testInitClickhouseRepo() failed", err))
+		os.Exit(constant.DefaultAbnormalExitCode)
+	}
+	monitorSystem, err := mysqlServerService.GetMySQLServers()[constant.ZeroInt].GetMonitorSystem()
+	if err != nil {
+		log.Error(common.CombineMessageWithError("testInitClickhouseRepo() failed", err))
+		os.Exit(constant.DefaultAbnormalExitCode)
+	}
+	addr := fmt.Sprintf("%s:%d", monitorSystem.GetHostIP(), monitorSystem.GetPortNumSlow())
+	dbUser := viper.GetString(config.DBMonitorClickhouseUserKey)
+	dbPass := viper.GetString(config.DBMonitorClickhousePassKey)
+	conn, err := clickhouse.NewConnWithDefault(addr, pmmClickhouseDBName, dbUser, dbPass)
+	if err != nil {
+		log.Error(common.CombineMessageWithError("testInitClickhouseRepo() failed", err))
+		os.Exit(constant.DefaultAbnormalExitCode)
+	}
+
+	return NewClickHouseRepo(NewConfigWithDefault(), conn)
+}
+
 func TestQueryRepositoryAll(t *testing.T) {
+	TestDASRepo_Save(t)
 	TestMySQLRepo_GetByServiceNames(t)
 	TestMySQLRepo_GetByDBName(t)
 	TestMySQLRepo_GetBySQLID(t)
-	TestClickhouseRepo_GetByDBName(t)
 	TestClickhouseRepo_GetByServiceNames(t)
+	TestClickhouseRepo_GetByDBName(t)
 	TestClickhouseRepo_GetBySQLID(t)
 }
 
-func init() {
-	viper.Set(config.DBMonitorMySQLUserKey, config.DefaultDBMonitorMySQLUser)
-	viper.Set(config.DBMonitorMySQLPassKey, config.DefaultDBMonitorMySQLPass)
+func TestDASRepo_Save(t *testing.T) {
+	asst := assert.New(t)
+
+	err := testDASRepo.Save(
+		testPMM2MySQLClusterID,
+		testPMM2MySQLServerID,
+		testPMM2DBID,
+		testPMM2SQLID,
+		time.Now().Add(-constant.Week),
+		time.Now(),
+		defaultLimit,
+		defaultOffset,
+	)
+	asst.Nil(err, common.CombineMessageWithError("test Save() failed", err))
 }
 
 func TestMySQLRepo_GetByServiceNames(t *testing.T) {
 	asst := assert.New(t)
 
-	q := NewQuerierWithGlobal(NewConfigWithDefault())
-	monitorSystem, err := q.getMonitorSystemByMySQLServerID(testMySQLServerID)
-
-	monitorRepo, err := q.getMonitorRepo(monitorSystem)
-	mr := monitorRepo
-	queries, err := mr.GetByServiceNames([]string{testServiceName})
-	asst.Nil(err, "test TestMySQLRepo_GetByServiceNames() Failed")
-	asst.NotZero(len(queries), "test TestMySQLRepo_GetByServiceNames() Failed")
+	queries, err := testMySQLRepo.GetByServiceNames([]string{testPMM1ServiceName})
+	asst.Nil(err, common.CombineMessageWithError("test GetByServiceNames() failed", err))
+	asst.GreaterOrEqual(len(queries), constant.ZeroInt, "test TestMySQLRepo_GetByServiceNames() failed")
 }
 
 func TestMySQLRepo_GetByDBName(t *testing.T) {
 	asst := assert.New(t)
 
-	q := NewQuerierWithGlobal(NewConfigWithDefault())
-	monitorSystem, err := q.getMonitorSystemByMySQLServerID(testMySQLServerID)
-
-	mr, err := q.getMonitorRepo(monitorSystem)
-	qu, err := mr.GetByDBName(testServiceName, testDBName)
-	asst.Equal(nil, err, "test MySQLRepo_GetByDBName() Failed")
-	asst.Equal(true, qu != nil, "test MySQLRepo_GetByDBName() Failed")
+	queries, err := testMySQLRepo.GetByDBName(testPMM1ServiceName, testPMM1DBName)
+	asst.Nil(err, common.CombineMessageWithError("test GetByDBName() failed", err))
+	asst.GreaterOrEqual(len(queries), constant.ZeroInt, "test GetByDBName() failed")
 }
 
 func TestMySQLRepo_GetBySQLID(t *testing.T) {
 	asst := assert.New(t)
 
-	q := NewQuerierWithGlobal(NewConfigWithDefault())
-	q.config.startTime = time.Now().Add(-constant.Week)
-
-	q.config.endTime = time.Now()
-	monitorSystem, err := q.getMonitorSystemByMySQLServerID(testMySQLServerID)
-
-	monitorRepo, err := q.getMonitorRepo(monitorSystem)
-
-	mr := monitorRepo
-	query, err := mr.GetBySQLID(testServiceName, testSQLID)
-	asst.Equal(nil, err, "test MySQLRepo_GetBySQLID() Failed")
-	asst.NotNil(query, "test MySQLRepo_GetBySQLID() Failed")
-}
-
-func TestClickhouseRepo_GetByDBName(t *testing.T) {
-	asst := assert.New(t)
-
-	q := NewQuerierWithGlobal(NewConfigWithDefault())
-	monitorSystem, err := q.getMonitorSystemByMySQLServerID(testMySQLServerID)
-
-	monitorRepo, err := q.getMonitorRepo(monitorSystem)
-
-	cr := monitorRepo
-	qu, err := cr.GetByDBName(testServiceName, testDBName)
-
-	asst.Equal(nil, err, "test ClickhouseRepo_GetByDBName() Failed")
-	asst.Equal(true, qu != nil, "test ClickhouseRepo_GetByDBName() Failed")
-
+	query, err := testMySQLRepo.GetBySQLID(testPMM1ServiceName, testPMM1SQLID)
+	asst.Nil(err, common.CombineMessageWithError("test GetBySQLID() failed", err))
+	asst.NotNil(query, "test GetBySQLID() failed")
 }
 
 func TestClickhouseRepo_GetByServiceNames(t *testing.T) {
 	asst := assert.New(t)
 
-	q := NewQuerierWithGlobal(NewConfigWithDefault())
-	monitorSystem, err := q.getMonitorSystemByMySQLServerID(testMySQLServerID)
+	queries, err := testClickhouseRepo.GetByServiceNames([]string{testPMM2ServiceName})
+	asst.Nil(err, common.CombineMessageWithError("test GetByServiceNames() failed", err))
+	asst.GreaterOrEqual(len(queries), constant.ZeroInt, "test GetByServiceNames() Failed")
+}
 
-	monitorRepo, err := q.getMonitorRepo(monitorSystem)
+func TestClickhouseRepo_GetByDBName(t *testing.T) {
+	asst := assert.New(t)
 
-	cr := monitorRepo
-	qu, err := cr.GetByServiceNames([]string{testServiceName})
-	asst.Equal(nil, err, "test ClickhouseRepo_GetByServiceNames() Failed")
-	asst.Equal(true, qu != nil, "test ClickhouseRepo_GetByServiceNames() Failed")
+	queries, err := testClickhouseRepo.GetByDBName(testPMM2ServiceName, testPMM2DBName)
+	asst.Nil(err, common.CombineMessageWithError("test GetByDBName() failed", err))
+	asst.GreaterOrEqual(len(queries), constant.ZeroInt, "test GetByDBName() Failed")
 }
 
 func TestClickhouseRepo_GetBySQLID(t *testing.T) {
 	asst := assert.New(t)
 
-	q := NewQuerierWithGlobal(NewConfigWithDefault())
-	q.config.startTime = time.Date(2021, 11, 1, 1, 1, 1, 1, time.Local)
-	q.config.endTime = time.Date(2021, 11, 30, 1, 1, 1, 1, time.Local)
-	monitorSystem, err := q.getMonitorSystemByMySQLServerID(testMySQLServerID)
-	monitorRepo, err := q.getMonitorRepo(monitorSystem)
-
-	cr := monitorRepo
-	qu, err := cr.GetBySQLID(testServiceName, testSQLID)
-	asst.Equal(nil, err, "test ClickhouseRepo_GetBySQLID() Failed")
-	asst.Equal(true, qu != nil, "test ClickhouseRepo_GetBySQLID() Failed")
+	query, err := testClickhouseRepo.GetBySQLID(testPMM2ServiceName, testPMM2SQLID)
+	asst.Nil(err, common.CombineMessageWithError("test GetBySQLID() failed", err))
+	asst.NotNil(query, "test GetBySQLID() Failed")
 }
--- internal/app/query/service_test.go
+++ internal/app/query/service_test.go
@@ -3,130 +3,35 @@ package query
 import (
 	"testing"
 
-	"github.com/romberli/das/config"
-	"github.com/romberli/das/global"
 	"github.com/romberli/go-util/common"
-	"github.com/romberli/go-util/middleware/mysql"
-	"github.com/romberli/log"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	defaultQuerySQLID           = "sql_id"
-	defaultQueryFingerprint     = "fingerprint"
-	defaultQueryExample         = "example"
-	defaultQueryDBName          = "test"
-	defaultQueryExecCount       = 1
-	defaultQueryTotalExecTime   = 3.5
-	defaultQueryAvgExecTime     = 1.5
-	defaultQueryRowsExaminedMax = 10
-
-	testPMM1ServiceName    = "192-168-10-220:3306"
-	testPMM1MySQLClusterID = 2
-	testPMM1MySQLServerID  = 2
-	testPMM1DBID           = 3
-	testPMM1DBName         = "sbtest"
-	testPMM1SQLID          = "8245FBA6EC962AA4"
-
-	testPMM2ServiceName    = "192-168-10-219:3306"
-	testPMM2MySQLClusterID = 1
-	testPMM2MySQLServerID  = 1
-	testPMM2DBID           = 1
-	testPMM2DBName         = "pmmtest"
-	testPMM2SQLID          = "5418BB5EE2F4A162"
-)
-
-var (
-	testServiceName    string
-	testMySQLClusterID int
-	testMySQLServerID  int
-	testDBID           int
-	testDBName         string
-	testSQLID          string
-	testService        *Service
-)
+var testService *Service
 
 func init() {
-	// pmmVersion = 1
-	pmmVersion = 2
+	testPMMVersion = 2
+	testInitMySQLInfo()
+	testInitDASMySQLPool()
+	testInitViper()
 
-	switch pmmVersion {
-	case 1:
-		testServiceName = testPMM1ServiceName
-		testMySQLClusterID = testPMM1MySQLClusterID
-		testMySQLServerID = testPMM1MySQLServerID
-		testDBID = testPMM1DBID
-		testDBName = testPMM1DBName
-		testSQLID = testPMM1SQLID
-		viper.Set(config.DBMonitorMySQLUserKey, config.DefaultDBMonitorMySQLUser)
-		viper.Set(config.DBMonitorMySQLPassKey, config.DefaultDBMonitorMySQLPass)
-	case 2:
-		testServiceName = testPMM2ServiceName
-		testMySQLClusterID = testPMM2MySQLClusterID
-		testMySQLServerID = testPMM2MySQLServerID
-		testDBID = testPMM2DBID
-		testDBName = testPMM2DBName
-		testSQLID = testPMM2SQLID
-		viper.Set(config.DBMonitorClickhouseUserKey, config.DefaultDBMonitorClickhouseUser)
-		viper.Set(config.DBMonitorClickhousePassKey, config.DefaultDBMonitorClickhousePass)
-	}
-
-	testService = createService()
-
-}
-
-func initQueryRepo() *DASRepo {
-	var err error
-
-	global.DASMySQLPool, err = mysql.NewPoolWithDefault(testDBAddr, testDBDBName, testDBDBUser, testDBDBPass)
-	if err != nil {
-		log.Error(common.CombineMessageWithError("initQueryRepo() failed", err))
-		return nil
-	}
-	return newDASRepo(global.DASMySQLPool)
-}
-
-func initNewQuery() *Query {
-	return &Query{
-		defaultQuerySQLID,
-		defaultQueryFingerprint,
-		defaultQueryExample,
-		defaultQueryDBName,
-		defaultQueryExecCount,
-		defaultQueryTotalExecTime,
-		defaultQueryAvgExecTime,
-		defaultQueryRowsExaminedMax,
-	}
-}
-
-func createService() *Service {
-	return newService(NewConfigWithDefault(), initQueryRepo())
+	testService = newService(NewConfigWithDefault(), testDASRepo)
 }
 
 func TestService_All(t *testing.T) {
-	TestService_GetConfig(t)
-	TestService_GetQueries(t)
+	testPMMVersion = 1
 	TestService_GetByMySQLClusterID(t)
 	TestService_GetByMySQLServerID(t)
 	TestService_GetByDBID(t)
 	TestService_GetBySQLID(t)
 	TestService_Marshal(t)
-}
 
-func TestService_GetConfig(t *testing.T) {
-	asst := assert.New(t)
-
-	limit := testService.GetConfig().GetLimit()
-	asst.Equal(defaultLimit, limit, "test GetConfig() failed")
-}
-
-func TestService_GetQueries(t *testing.T) {
-	asst := assert.New(t)
-
-	testService.queries = append(testService.queries, initNewQuery())
-	sqlID := testService.GetQueries()[0].GetSQLID()
-	asst.Equal(defaultQuerySQLID, sqlID, "test GetQueries() failed")
+	testPMMVersion = 2
+	TestService_GetByMySQLClusterID(t)
+	TestService_GetByMySQLServerID(t)
+	TestService_GetByDBID(t)
+	TestService_GetBySQLID(t)
+	TestService_Marshal(t)
 }
 
 func TestService_GetByMySQLClusterID(t *testing.T) {
@@ -160,6 +65,7 @@ func TestService_GetBySQLID(t *testing.T) {
 func TestService_Marshal(t *testing.T) {
 	asst := assert.New(t)
 
-	_, err := testService.Marshal()
+	jsonBytes, err := testService.Marshal()
 	asst.Nil(err, common.CombineMessageWithError("test Marshal() failed", err))
+	t.Log(string(jsonBytes))
 }
