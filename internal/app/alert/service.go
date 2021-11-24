65bdb4fd6118cb6299253c8237d24c5c0fcacf52
Romber Li <romber2001@163.com>
2021-11-24 19:40:56 +0800

    healthcheck: fix sending email via http

--- internal/app/alert/service.go
+++ internal/app/alert/service.go
@@ -146,6 +146,7 @@ func (s *Service) setupSMTPConfig(toAddrs, ccAddrs, subject, content string) {
 // setupHTTPConfig setups the HTTP config
 func (s *Service) setupHTTPConfig(toAddrs, ccAddrs, content string) {
 	s.GetConfig().Set(toAddrsJSON, strings.Trim(toAddrs+constant.CommaString+ccAddrs, constant.CommaString))
+	s.GetConfig().Set(ccAddrsJSON, ccAddrs)
 	s.GetConfig().Set(contentJSON, content)
 }
 
