a712490d3c7e0d6c74a5bd9d7eca92814e223481
Romber Li <romber2001@163.com>
2021-11-26 16:21:02 +0800

    healthcheck: fix column alias for getting table size

--- internal/app/healthcheck/query.go
+++ internal/app/healthcheck/query.go
@@ -11,7 +11,7 @@ const (
     	select table_schema,
 			   table_name,
 			   table_rows,
-			   truncate((data_length + index_length) / 1024 / 1024 / 1024, 2) as size
+			   truncate((data_length + index_length) / 1024 / 1024 / 1024, 2) as table_size
 		from information_schema.tables
 		where table_type = 'BASE TABLE'
 		  and table_rows > ?
