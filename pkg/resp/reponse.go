77c42423fdcbfeef720a7759440880518b07593e
Romber Li <romber2001@163.com>
2022-03-04 16:15:29 +0800

    gomod: update dependent package response: modify response message

--- pkg/resp/reponse.go
+++ pkg/resp/reponse.go
@@ -1,6 +1,7 @@
 package resp
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,8 @@ import (
 	"github.com/romberli/das/pkg/message"
 )
 
+const responseNOKMessage = `{"message": "%s"}`
+
 // ResponseNOK responses with given code and values,
 // if code is between 400000 and 500000, it will log error and resp 500 to client
 // otherwise, it will log info and resp 200 to client
@@ -16,7 +19,7 @@ func ResponseNOK(c *gin.Context, code int, values ...interface{}) {
 	err := message.NewMessage(code, values...)
 	log.Errorf("%+v", err)
 
-	c.String(http.StatusInternalServerError, err.Error())
+	c.String(http.StatusInternalServerError, fmt.Sprintf(responseNOKMessage, err.Error()))
 }
 
 func ResponseOK(c *gin.Context, respMessage string, code int, values ...interface{}) {
