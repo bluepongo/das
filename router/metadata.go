97f7a7a2743cbe4012e5fd49a6bc8cd0db8925b6
SpicyChickenFLY <xixiayuchao@gmail.com>
2022-03-16 22:56:45 +0800

    init metadata/table in router

--- router/metadata.go
+++ router/metadata.go
@@ -121,5 +121,11 @@ func RegisterMetadata(group *gin.RouterGroup) {
 		metadataGroup.GET("/user/middleware-cluster/:id", metadata.GetMiddlewareClustersByUserID)
 		metadataGroup.GET("/user/mysql-cluster/:id", metadata.GetMySQLClustersByUserID)
 		metadataGroup.GET("/user/all-mysql-server/:id", metadata.GetAllMySQLServersByUserID)
+		// table
+		metadataGroup.GET("/table/db/:id", metadata.GetTablesByDBID)
+		metadataGroup.GET("/table/statistic/db-table", metadata.GetTableByDBIDAndTableName)
+		metadataGroup.GET("/table/statistic/host-info-db-table", metadata.GetTableByHostInfoAndDBNameAndTableName)
+		// metadataGroup.POST("/table/analyze/db", metadata.AnalyzeTableByDBIDAndTableName)
+		// metadataGroup.POST("/table/analyze/host-info", metadata.AnalyzeTableByHostInfoAndDBNameAndTableName)
 	}
 }
