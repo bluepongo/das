eb10cc9e3af0ded9e7f1aa50a2f4822d62a4be98
gengxiaoyun <gengxiaoyun@shu.edu.cn>
2022-03-17 11:37:58 +0800

    update db

--- pkg/util/metadata/metadata.go
+++ pkg/util/metadata/metadata.go
@@ -5,24 +5,18 @@ type HostInfo struct {
 	PortNum int    `json:"port_num" bind:"required"`
 }
 
-type NameAndClusterInfo struct {
-	DBName      string `json:"db_name" bind:"required"`
-	ClusterID   int    `json:"cluster_id" bind:"required"`
-	ClusterType int    `json:"cluster_type" bind:"required"`
-}
-
-type NameAndHostInfo struct {
-	DBName  string `json:"db_name" bind:"required"`
-	HostIP  string `json:"host_ip" bind:"required"`
-	PortNum int    `json:"port_num" bind:"required"`
-}
-
 func (hi *HostInfo) GetHostIP() string {
 	return hi.HostIP
 }
 
 func (hi *HostInfo) GetPortNum() int {
 	return hi.PortNum
+}
+
+type NameAndClusterInfo struct {
+	DBName      string `json:"db_name" bind:"required"`
+	ClusterID   int    `json:"cluster_id" bind:"required"`
+	ClusterType int    `json:"cluster_type" bind:"required"`
 }
 
 func (nc *NameAndClusterInfo) GetDBName() string {
@@ -35,6 +29,12 @@ func (nc *NameAndClusterInfo) GetClusterID() int {
 
 func (nc *NameAndClusterInfo) GetClusterType() int {
 	return nc.ClusterType
+}
+
+type NameAndHostInfo struct {
+	DBName  string `json:"db_name" bind:"required"`
+	HostIP  string `json:"host_ip" bind:"required"`
+	PortNum int    `json:"port_num" bind:"required"`
 }
 
 func (nh *NameAndHostInfo) GetDBName() string {
